090acc648742a798c4944f2ca10ae0cac419a5de
Glitch Mastodon Test <mastodon@dev.kafuka.me>
2019-08-11 14:08:35 -0400

    Adjusting base image resolution to 2560x2560

--- app/models/media_attachment.rb
+++ app/models/media_attachment.rb
@@ -42,7 +42,7 @@ class MediaAttachment < ApplicationRecord
 
   IMAGE_STYLES = {
     original: {
-      pixels: 1_638_400, # 1280x1280px
+      pixels: 6_553_600, # 2560x2560px
       file_geometry_parser: FastGeometryParser,
     },
 
